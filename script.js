f43b45a643364418c7c9458302e7553642c28030
mardiev.dev <mardiev2125@gmail.com>
2022-03-02 22:51:25 -0600

    month added in Analog Clock

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var hourEl = document.querySelector(".hour");
 var minEl = document.querySelector(".min");
 var secEl = document.querySelector(".sec");
+var dateEl = document.querySelector(".date");
 
 setInterval(function analogClock() {
     var currentTime = new Date();
@@ -8,10 +9,11 @@ setInterval(function analogClock() {
     var secDeg = (currentTime.getSeconds() / 60) * 360 - 90;
     var minDeg = (currentTime.getMinutes() / 60) * 360 - 90;
     var hourDeg = (currentTime.getHours() / 12) * 360 - 90;
-
+    var dateDeg = currentTime.getMonth() + 1;
     secEl.style.transform = `rotate(${secDeg}deg)`;
     minEl.style.transform = `rotate(${minDeg}deg)`;
     hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    dateEl.innerHTML = dateDeg;
 }, 1000);
 
 analogClock();
